a410ba0b33f668ae222258df5464dc3c8a62c0ab
gpmlgpmll-alt <gpmlgpmll@gmail.com>
2025-09-16 07:41:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,19 @@
 import telebot
-import os
 
-# توكن البوت (من BotFather)
-TOKEN = os.environ.get("BOT_TOKEN")
+# توكن البوت من BotFather
+TOKEN = "6991340590:AAHExa9Idc3OB-IeuQy_7dU-3DJ1KymMH_c"
 
 bot = telebot.TeleBot(TOKEN)
 
+# رسالة الترحيب
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "هلا بيك! ✨ البوت يشتغل تمام 🚀")
 
+# يكرر أي رسالة يكتبها المستخدم
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
 
-print("البوت يشتغل...")
+print("..البوت يشتغل")
 bot.infinity_polling()
